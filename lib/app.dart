164501c88439af86db4e1beb638497daa9265a0a
Shraddha <mailmeatshraddha@gmail.com>
2022-09-16 17:57:20 -0400

    feat: set app bar background color

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'strings.dart';
+import 'ui/courses/courses_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
+    return MaterialApp(
+      title: Strings.appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).copyWith(
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: Colors.green[800],
+            ),
+      ),
+      home: SafeArea(
+        child: Scaffold(
           appBar: AppBar(
-        title: Text(Strings.appTitle),
-      )),
+            title: Text(Strings.appTitle),
+          ),
+          body: const CoursesPage(),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'app.dart';
-import 'strings.dart';
 
-void main() => (MaterialApp(
-      title: Strings.appTitle,
-      theme: ThemeData(primaryColor: Colors.green[800]),
-      home: const App(),
-    ));
+void main() => runApp(const App());
