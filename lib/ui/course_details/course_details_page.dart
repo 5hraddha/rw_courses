1d6353619d7780049f544d6528baee4dd813038a
Shraddha <mailmeatshraddha@gmail.com>
2022-09-17 16:23:34 -0400

    feat: complete the course detail course page

--- lib/ui/course_details/course_details_page.dart
+++ lib/ui/course_details/course_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../model/course.dart';
+import '../../utils/string_extensions.dart';
+import 'image_container.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({
@@ -16,7 +18,99 @@ class CourseDetails extends StatelessWidget {
         appBar: AppBar(
           title: Text(course.name),
         ),
-        body: Text(course.description),
+        // https://api.flutter.dev/flutter/widgets/SingleChildScrollView-class.html
+        body: LayoutBuilder(builder:
+            (BuildContext context, BoxConstraints viewportConstraints) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.green[50],
+            ),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _buildBanner(),
+                    _buildMain(context),
+                    _buildDetails(context),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildBanner() {
+    return ImageContainer(
+      height: 200,
+      url: course.artworkUrl,
+    );
+  }
+
+  Widget _buildMain(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+            child: Text(
+              course.name,
+              style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                    fontSize: 24,
+                    color: Colors.green[900],
+                  ),
+            ),
+          ),
+          Text(
+            course.description,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetails(BuildContext context) {
+    const style = TextStyle(fontSize: 16.0);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Domain(s): ${course.domainString}',
+            style: style,
+          ),
+          Text(
+            'Level: ${course.difficulty.capitalize()}',
+            style: style,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              '${course.contributors}',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.green[900],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
