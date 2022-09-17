86001da1038d65c36488e73e46c2d6e258a9dfa7
Shraddha <mailmeatshraddha@gmail.com>
2022-09-16 20:29:57 -0400

    feat: add more fields to the course model

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -2,15 +2,28 @@ class Course {
   final String courseId;
   final String name;
   final String description;
+  final String difficulty;
+  final String contributors;
+  final String artworkUrl;
 
-  Course(this.courseId, this.name, this.description);
+  Course(
+    this.courseId,
+    this.name,
+    this.description,
+    this.difficulty,
+    this.contributors,
+    this.artworkUrl,
+  );
 
   // Named contructor returns an object of Course class using values from json
   Course.fromJson(Map<String, dynamic> json)
       : courseId = json['id'] as String,
         name = json['attributes']['name'] as String,
-        description = json['attributes']['description_plain_text'] as String;
+        description = json['attributes']['description_plain_text'] as String,
+        difficulty = json['attributes']['difficulty'],
+        contributors = json['attributes']['contributor_string'],
+        artworkUrl = json['attributes']['card_artwork_url'];
 
   @override
-  String toString() => name;
+  String toString() => name + ' : ' + difficulty;
 }
