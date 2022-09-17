65c3111316f6c7d17c011dd2d46cd76b62402623
Shraddha <mailmeatshraddha@gmail.com>
2022-09-17 17:56:43 -0400

    feat: add a filter page

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'strings.dart';
 import 'ui/courses/courses_page.dart';
+import 'ui/filter/filter_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -8,21 +9,33 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: Strings.appTitle,
-      debugShowCheckedModeBanner: false,
-      theme: Theme.of(context).copyWith(
-        appBarTheme: Theme.of(context).appBarTheme.copyWith(
-              backgroundColor: Colors.green[800],
-            ),
-      ),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(Strings.appTitle),
-          ),
-          body: const CoursesPage(),
+        title: Strings.appTitle,
+        debugShowCheckedModeBanner: false,
+        theme: Theme.of(context).copyWith(
+          appBarTheme: Theme.of(context).appBarTheme.copyWith(
+                backgroundColor: Colors.green[800],
+              ),
         ),
-      ),
-    );
+        home: Builder(
+          builder: (context) => SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text(Strings.appTitle),
+                actions: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const FilterPage()),
+                        );
+                      },
+                      icon: const Icon(Icons.filter_list)),
+                ],
+              ),
+              body: const CoursesPage(),
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/ui/filter/filter_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../strings.dart';
+
+class FilterPage extends StatefulWidget {
+  const FilterPage({Key? key}) : super(key: key);
+
+  @override
+  State<FilterPage> createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(Strings.filter),
+        ),
+        body: const Text('Filter Page'),
+      ),
+    );
+  }
+}
